816f65740b648b2272de19547e9fd9a8911c8433
Milo <emilio.figueroa@happyshop.cl>
2014-03-14 23:11:24 -0300

    Configuration files

--- lita_config.rb
+++ lita_config.rb
@@ -1,22 +1,20 @@
 Lita.configure do |config|
-  # The name your robot will use.
-  config.robot.name = 'Lita'
 
-
-  ### SLACK
-  config.robot.name = 'Damnbot'
-  config.robot.mention_name = '@damnbot'
-  config.robot.alias = '/'
-  # lita-slack adapter config
-  #config.adapter.incoming_url = 'https://bbrmobile.slack.com/services/hooks/incoming-webhook'
-  #config.adapter.username = 'damnbot'
-  #config.adapter.add_mention = false
-  #config.adapter.incoming_token = 'jH0ZZePpgy2XTJv03tJSSMJ2'
-  #config.adapter.team_domain = 'bbrmobile'
-  #config.adapter.username = 'damnbot'
+  robot_config = OpenStruct.new YAML.load_file("#{File.dirname(__FILE__)}/config/robot.yml")
+  config.robot.name = robot_config.name
+  config.robot.mention_name = robot_config.mention_name
+  config.robot.alias = robot_config.alias
 
   # The locale code for the language to use.
-  config.robot.locale = :es
+  config.robot.locale = robot_config.locale
+
+  ### lita-slack adapter config
+  # slack_config = OpenStruct.new YAML.load_file("#{File.dirname(__FILE__)}/config/slack.yml")
+  # config.adapter.incoming_url = slack_config.incoming_url
+  # config.adapter.username = slack_config.username
+  # config.adapter.add_mention = slack_config.add_mention
+  # config.adapter.incoming_token = slack_config.incoming_token
+  # config.adapter.team_domain = slack_config.team_domain
 
   # The severity of messages to log. Options are:
   # :debug, :info, :warn, :error, :fatal
@@ -26,18 +24,19 @@ Lita.configure do |config|
   # An array of user IDs that are considered administrators. These users
   # the ability to add and remove other users from authorization groups.
   # What is considered a user ID will change depending on which adapter you use.
-  config.robot.admins = ["1", "2"]
+  config.robot.admins = robot_config.admins 
 
   # The adapter you want to connect with. Make sure you've added the
   # appropriate gem to the Gemfile.
   config.robot.adapter = :console # :shell, :console, :slack
 
-  #config.http.port = 111111
+  config.http.port = robot_config.port unless [:shell, :console].include? config.robot.adapter
 
 
-  ## Example: Set options for the Redis connection.
-  config.redis.host = '127.0.0.1'
-  config.redis.port = 6379
+  # Set options for the Redis connection.
+  redis_config = OpenStruct.new YAML.load_file('config/redis.yml')
+  config.redis.host = redis_config.host
+  config.redis.port = redis_config.port
 
   config.handlers.github_commits.repos = {
     "username/repo1" => "#someroom",
@@ -46,11 +45,17 @@ Lita.configure do |config|
 
   config.handlers.google_images.safe_search = :off
 
-  config.handlers.jenkins.url  = "http://test.com"
-  config.handlers.jenkins.auth = "user1:sekret"
+  jenkins_config = OpenStruct.new YAML.load_file('config/jenkins.yml')
+  config.handlers.jenkins.url  = jenkins_config.url
+  config.handlers.jenkins.auth = jenkins_config.auth
 
-  config.handlers.memegen.command_only = false
-  config.handlers.memegen.username = 'bbr@yopmail.com'
-  config.handlers.memegen.password = 'bbr'
+  memegen_config = OpenStruct.new YAML.load_file("#{File.dirname(__FILE__)}/config/memegen.yml")
+  config.handlers.memegen.command_only = memegen_config.command_only
+  config.handlers.memegen.username = memegen_config.username
+  config.handlers.memegen.password = memegen_config.password
 
 end
+
+Lita.load_locales Dir[File.expand_path(
+                          File.join("..", "..", "locales", "*.yml"), __FILE__
+                      )]
